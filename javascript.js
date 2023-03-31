e6be96411d818737c10aac4155a345a3750bc5b3
Yasin-HH <pro.harchaouiyasin@gmail.com>
2023-03-31 23:38:51 +0200

    Animation (JS) + Txt upgrade + Nav Accueil

--- javascript.js
+++ javascript.js
@@ -12,3 +12,68 @@ projectImages.forEach((image) => {
   });
 });
 
+/* Animation */
+let textElement = document.getElementById('dynamic-text');
+
+let baseText = "Je suis étudiant en informatique passionné par ";
+let steps = ["le réseau", "le système", "la cybersécurité"];
+
+let currentStep = 0;
+let currentChar = 0;
+let erasing = false;
+let initialWrite = true;
+
+function updateText() {
+  // Écrire la phrase complète au début
+  if (initialWrite) {
+    if (currentChar < baseText.length + steps[currentStep].length) {
+      // Écrire le texte de base
+      if (currentChar < baseText.length) {
+        textElement.textContent += baseText[currentChar];
+      }
+      // Écrire la partie finale de la phrase
+      else {
+        textElement.textContent += steps[currentStep][currentChar - baseText.length];
+      }
+      currentChar++;
+      setTimeout(updateText, 50);
+    } else {
+      // Passer à l'animation d'écriture et d'effacement
+      initialWrite = false;
+      erasing = true;
+      setTimeout(updateText, 1000);
+    }
+  }
+  // Ajouter des caractères à la fin
+  else if (!erasing) {
+    if (currentChar < steps[currentStep].length) {
+      textElement.textContent = baseText + steps[currentStep].substring(0, currentChar + 1);
+      currentChar++;
+      setTimeout(updateText, 50);
+    } else {
+      // Passer à l'effacement
+      erasing = true;
+      setTimeout(updateText, 1000);
+    }
+  }
+  // Effacer des caractères à la fin
+  else {
+    if (currentChar >= 0) {
+      textElement.textContent = baseText + steps[currentStep].substring(0, currentChar);
+      currentChar--;
+      setTimeout(updateText, 50);
+    } else {
+      // Passer à l'ajout de caractères
+      erasing = false;
+      currentStep = (currentStep + 1) % steps.length;
+      setTimeout(updateText, 500);
+    }
+  }
+}
+
+// Lancer la fonction d'animation
+updateText();
+
+
+
+
