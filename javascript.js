464c3474969b62dde217e220a2ee328d329cd0bb
Yasin-HH <pro.harchaouiyasin@gmail.com>
2023-04-01 11:39:17 +0200

    add little btn for nav+ animation of project

--- javascript.js
+++ javascript.js
@@ -121,3 +121,13 @@ challenges.forEach((challenge) => {
   progressBar.style.width = progressPercentage + '%';
 });
 
+
+const menuIcon = document.querySelector('.menu-icon');
+const nav = document.querySelector('nav');
+
+menuIcon.addEventListener('click', () => {
+  menuIcon.classList.toggle('active');
+  nav.classList.toggle('active');
+});
+
+
